9fe8774fb1ea788b1c37eab97a1f2ad8e33e5f0d
Noël Bossart <noel.bossart@namics.com>
2013-09-19 16:43:32 +0200

    Adding Less (Not Working)

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     jade = require('jade'),
+    lessMiddleware = require('less-middleware'),
     port = 3000;
     
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
 app.configure(function() {
-    app.use(
-       express.static(__dirname + '/public')
-   );
+     app.use(lessMiddleware({
+       src      : __dirname + "/public/modules/space",
+       compress : false,
+       force: true
+     }));
+     app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function(req, res){
