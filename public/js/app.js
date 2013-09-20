7de0d7fc853922498e9aadd75e0f3a04785b8b0d
Noël Bossart <noel.bossart@namics.com>
2013-09-20 15:56:19 +0200

    Added Space

--- public/js/app.js
+++ public/js/app.js
@@ -17,19 +17,103 @@ $(document).ready(function(){
 })(jQuery);
 
 
+/* space */
+// The amount of symbol we want to place;
+var count = 30;
+
+// Create a symbol, which we will use to place instances of later:
+var path = new Path.Circle({
+	center: [0, 0],
+	radius: 5,
+	fillColor: '#ddd',
+	strokeColor: '#ddd'
+});
+
+var symbol = new Symbol(path);
+
+// Place the instances of the symbol:
+for (var i = 0; i < count; i++) {
+	// The center position is a random point in the view:
+	var center = Point.random() * view.size;
+	var placed = symbol.place(center);
+	placed.scale(i / count + 0.001);
+	placed.data.vector = new Point({
+		angle: Math.random() * 360,
+		length : (i / count) * Math.random() / 5
+	});
+}
+
+var vector = new Point({
+	angle: 45,
+	length: 0
+});
+
+var mouseVector = vector.clone();
+
+function onMouseMove(event) {
+	mouseVector = view.center - event.point;
+}
+
+function keepInView(item) {
+	var position = item.position;
+	var viewBounds = view.bounds;
+	if (position.isInside(viewBounds))
+		return;
+	var itemBounds = item.bounds;
+	if (position.x > viewBounds.width + 5) {
+		position.x = -item.bounds.width;
+	}
+
+	if (position.x < -itemBounds.width - 5) {
+		position.x = viewBounds.width;
+	}
+
+	if (position.y > viewBounds.height + 5) {
+		position.y = -itemBounds.height;
+	}
+
+	if (position.y < -itemBounds.height - 5) {
+		position.y = viewBounds.height
+	}
+}
+
+/* Sperm */
 
 function onFrame() {
-	if (Key.isDown('left'))
+	if (Key.isDown('left')){
+    	vector = vector + (new Point(10,0) - vector) / 30;
 		sperm.left();
+	    
+	}
 
-	if (Key.isDown('right'))
+	if (Key.isDown('right')){
+    	vector = vector + (new Point(-10,0) - vector) / 30;
 		sperm.right();
+	    
+	}
 
-	if (Key.isDown('up'))
+	if (Key.isDown('up')){
+    	vector = vector + (new Point(0,-10) - vector) / 30;
 		sperm.forward();
+	}
 
-	if (Key.isDown('down'))
+	if (Key.isDown('down')){
+    	vector = vector + (new Point(0,10) - vector) / 30;
 		sperm.reverse();
+	    
+	}
+    
+        
+	// Run through the active layer's children list and change
+	// the position of the placed symbols:
+	for (var i = 0; i < count; i++) {
+		var item = project.activeLayer.children[i];
+		var size = item.bounds.size;
+		var length = vector.length / 10 * size.width / 10;
+		item.position += vector.normalize(length) + item.data.vector;
+		keepInView(item);
+	}
+    
 	sperm.draw();
 }
 
